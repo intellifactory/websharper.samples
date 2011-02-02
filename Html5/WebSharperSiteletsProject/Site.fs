6bfaf55c0def7485ed9119e27ece078dbb59def9
Vladimir Matveev@localhost <Vladimir Matveev@localhost>
2011-02-03 00:48:25 +0200

    updated to the latest WS version
    --HG--
    rename : Basic/WebSharperTemplates/SamplePage.template.xml => Basic/WebSharperSiteletsProject/SamplePage.template.xml
    rename : Html5/WebSharperTemplates/SamplePage.template.xml => Html5/WebSharperSiteletsProject/SamplePage.template.xml

--- Html5/WebSharperSiteletsProject/Site.fs
+++ Html5/WebSharperSiteletsProject/Site.fs
@@ -8,38 +8,62 @@ open IntelliFactory.Html
 open IntelliFactory.WebSharper.Sitelets
 open IntelliFactory.WebSharper
 
-type Marker = class end
+type Samples = 
+    | Index
+    | Sample of string
 
-type SamplesList() = 
-    static let samples = 
-            typeof<Marker>.Assembly.GetTypes() 
-            |> Seq.filter (fun t -> typeof<Web.Control>.IsAssignableFrom t)
-            |> Seq.map (fun t -> 
-                let name = t.Name.Substring(0, t.Name.Length - "Viewer".Length)
-                name, t
-                )
-            |> List.ofSeq
-    static member GetSampleNames() = samples |> Seq.map fst |> Seq.toArray
-    static member internal GetSamples() = samples
-
+module Controls = 
+    let list = 
+        typeof<Samples>.Assembly.GetTypes() 
+        |> Seq.filter (fun t -> typeof<Web.Control>.IsAssignableFrom t)
+        |> Seq.map (fun t -> 
+            let name = t.Name.Substring(0, t.Name.Length - "Viewer".Length)
+            name, t
+            )
+        |> List.ofSeq
 
 module Site = 
-    let pages () =
-        SamplesList.GetSamples()
+    let pages =
+        Controls.list
             |> List.map (fun (name, t) ->
                 let el = Activator.CreateInstance(t) :?> Web.Control
-                Website.Templates.SamplePage.SamplePage name name <|
-                    {
-                        Caption = fun _ -> Div [Text name]
-                        Content = fun _ -> Div [el]
-                    }
+                let page = Website.Templates.Templates.SamplePage.SamplePage (Some name) <|
+                                {
+                                    Back = fun ctx -> [A [HRef (ctx.Link Index)] -< [Text "Back"]]
+                                    Caption = fun _ -> [Div [Text name]]
+                                    Content = fun _ -> [Div [el]]
+                                }
+                name, page
                 )
+            |> Map.ofSeq
+    let index = Website.Templates.Templates.SamplePage.SamplePage (Some "Index") <|
+                    {
+                        Back = fun _ -> []
+                        Caption = fun _ -> [Div [Text "Samples"]]
+                        Content = fun ctx ->
+                            [
+                                Table [
+                                    for name, _ in Controls.list do 
+                                        yield TR [ TD [ A [HRef (ctx.Link (Sample name))] -< [Text name] ]]
+                                ]
+                            ]
+                    }
         
-    
+    let controller = 
+        let handler = function
+            | Index -> index
+            | Sample s -> pages.[s]
+
+        { Handle = handler }         
 
 type Website() =
-    interface IWebsite with
-        member this.Pages = Site.pages ()           
+    interface IWebsite<Samples> with
+        member this.Actions = []
+        member this.Sitelet = 
+            {
+                Controller = Site.controller
+                Router = Router.Table [Index, "/"] <|> Router.Infer()
+            }           
 
 
 [<assembly: WebsiteAttribute(typeof<Website>)>]
